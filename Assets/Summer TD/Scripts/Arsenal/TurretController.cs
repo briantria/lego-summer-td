e97b432d0d17c4a6696fabab77a18fbdb2caaf5a
brian <brian.tria.se@gmail.com>
2021-05-29 16:34:43 +0400

    control turrent

--- Assets/Summer TD/Scripts/Arsenal/TurretController.cs
+++ Assets/Summer TD/Scripts/Arsenal/TurretController.cs
@@ -8,9 +8,20 @@ namespace Lego.SummerJam.NoFrogsAllowed
     {
         [SerializeField] private Transform _turretPivot;
 
-        private void Update()
-        {
+        [Space(8)]
+        [SerializeField] private float _horizontalSpeed = 2.0F;
+        [SerializeField] private float _verticalSpeed = 2.0F;
 
+        void Update()
+        {
+            float h = _horizontalSpeed * Input.GetAxis("Mouse X");
+            transform.Rotate(0, h, 0);
+
+            if (_turretPivot != null)
+            {
+                float v = _verticalSpeed * Input.GetAxis("Mouse Y");
+                _turretPivot.Rotate(v, 0, 0);
+            }
         }
     }
 }
