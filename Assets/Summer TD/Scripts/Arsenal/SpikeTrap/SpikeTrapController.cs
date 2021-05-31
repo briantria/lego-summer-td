382c15bfe1916e1465f725494e65e07db4c55c5e
brian <brian.tria.se@gmail.com>
2021-05-31 22:13:44 +0400

    show/hide spike traps

---
+++ Assets/Summer TD/Scripts/Arsenal/SpikeTrap/SpikeTrapController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+using Unity.LEGO.Game;
+
+namespace Lego.SummerJam.NoFrogsAllowed
+{
+    [RequireComponent(typeof(CustomAction))]
+    public class SpikeTrapController : MonoBehaviour, IAction
+    {
+        [SerializeField] private GameObject _spikeSellerObj;
+        [SerializeField] private GameObject _spikeTrapObj;
+
+        private void Start()
+        {
+            ShowSpikeSeller();
+        }
+
+        private void ShowSpikeSeller()
+        {
+            _spikeSellerObj.SetActive(true);
+            _spikeTrapObj.SetActive(false);
+        }
+
+        private void ShowSpikeTrap()
+        {
+            _spikeSellerObj.SetActive(false);
+            _spikeTrapObj.SetActive(true);
+        }
+
+        public void Activate()
+        {
+            ShowSpikeTrap();
+        }
+    }
+}
